6a03e7189cfd320491b269342e6c3856989b341c
Majta777 <60510110+Majta777@users.noreply.github.com>
2024-02-08 12:41:53 +0100

    Update sekretKacper.js

--- sekretKacper.js
+++ sekretKacper.js
@@ -1,18 +1,3 @@
-fetch('https://cors.szurek.local/10/', {
-    method: 'GET', // lub 'POST', w zależności od wymagań API
-    headers: {
-        // Dodaj nagłówki, jeśli są wymagane przez API
-    }
-})
-.then(response => {
-    if (response.ok) {
-        return response.text(); // lub response.json(), jeśli oczekiwany jest format JSON
-    }
-    throw new Error('Nie udało się pobrać sekretu');
-})
-.then(data => {
-    console.log('Sekret:', data);
-})
-.catch(error => {
-    console.error('Błąd:', error);
-});
+$.getJSON("https://cors.szurek.local/10/", function(json) {
+        alert(`sekret to ${json['sekret']}.`);
+    });
